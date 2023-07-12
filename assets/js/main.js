d396eb8efb55e7614a7d19452467e82d748c4283
Szabó Ármin András <46499638+lonelymous@users.noreply.github.com>
2023-07-12 03:08:10 +0200

    first version of my portfolio website

---
+++ assets/js/main.js
@@ -0,0 +1,79 @@
+
+window.onload = function() {
+    var birthday = new Date('2004-05-01');
+  
+    var today = new Date();
+    var age = today.getFullYear() - birthday.getFullYear();
+  
+    // Check if the birthday hasn't occurred yet this year
+    if (
+      today.getMonth() < birthday.getMonth() ||
+      (today.getMonth() === birthday.getMonth() && today.getDate() < birthday.getDate())
+    ) {
+      age--;
+    }
+  
+    var elements = document.getElementsByClassName('age-span'); 
+
+    [...elements].forEach(function(element) {
+        element.textContent = age;
+    });
+};
+
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav_link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 1000,
+    delay: 200,
+    reset: false
+});
+
+sr.reveal('.home_data, .about_img, .skills_subtitle, .skills_text, .section-title',{}); 
+sr.reveal('.home_img, .about_name, .about_occupation, .about_profile, .about_text, .skills_img, .text',{delay: 400}); 
+sr.reveal('.home_social-icon',{interval: 50}); 
+sr.reveal('.skills_data',{interval: 50});
+sr.reveal('.projects_img, .SxvContainer, .SxvButton',{interval: 50});
